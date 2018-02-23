0f00e04bd2c7e65597799f8d592f230a22a04e85
Jacob Polloreno <jpollore@e1z3r4p6.42.us.org>
2018-02-22 16:48:27 -0800

    make checks more readable

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -6,11 +6,23 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 09:13:56 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/22 11:57:27 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/22 16:41:08 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char *dst_s;
+	char *src_s;
+
+	dst_s = (char *)dst;
+	src_s = (char *)src;
+	while (n--)
+		*dst_s++ = *src_s++;
+	return (dst);
+}
 
 /* void	*ft_memcpy(void *dst, const void *src, size_t n) */
 /* { */
@@ -25,23 +37,23 @@
 /* 	return (dst); */
 /* } */
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	int *s_word;
-	int d_word;
-	int *dst32;
+/* void	*ft_memcpy(void *dst, const void *src, size_t n) */
+/* { */
+/* 	int *s_word; */
+/* 	int d_word; */
+/* 	int *dst32; */
 
-	s_word = (int *)src;
-	src++;
-	dst32 = (int *)dst;
-	while (n--)
-	{
-		d_word = *s_word << 8;
-		s_word = (int *)src;
-		src++;
-		d_word |= *s_word >> 24;
-		*dst32 = d_word;
-		dst++;
-	}
-	return (dst);
-}
+/* 	s_word = (int *)src; */
+/* 	src++; */
+/* 	dst32 = (int *)dst; */
+/* 	while (n--) */
+/* 	{ */
+/* 		d_word = *s_word << 8; */
+/* 		s_word = (int *)src; */
+/* 		src++; */
+/* 		d_word |= *s_word >> 24; */
+/* 		*dst32 = d_word; */
+/* 		dst++; */
+/* 	} */
+/* 	return (dst); */
+/* } */

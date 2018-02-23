ed5df9e0130f800c7aadb203a032fb47788d4703
Jacob Polloreno <jpollore@e1z3r4p6.42.us.org>
2018-02-22 17:56:10 -0800

    update test cases

--- srcs/ft_strcpy.c
+++ srcs/ft_strcpy.c
@@ -6,22 +6,32 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 18:33:21 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/22 11:57:31 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/22 17:46:30 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dest, char *src)
+/* char	*ft_strcpy(char *dest, char *src) */
+/* { */
+/* 	int len; */
+
+/* 	len = 0; */
+/* 	while (src[len]) */
+/* 	{ */
+/* 		dest[len] = src[len]; */
+/* 		len++; */
+/* 	} */
+/* 	dest[len] = '\0'; */
+/* 	return (dest); */
+/* } */
+
+char *ft_strcpy(char *dest, char *src)
 {
 	int len;
 
-	len = 0;
-	while (src[len])
-	{
-		dest[len] = src[len];
-		len++;
-	}
-	dest[len] = '\0';
+	len = ft_strlen(src);
+	ft_memcpy(dest, src, len);
+	dest[++len] = '\0';
 	return (dest);
 }

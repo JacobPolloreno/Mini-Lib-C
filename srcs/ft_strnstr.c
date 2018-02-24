6c217b5d88fc9d2b18c2b2befed774522dd8afa7
Jacob Polloreno <jpollore@e1z3r5p8.42.us.org>
2018-02-23 22:12:51 -0800

    fix test cases for strstr

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 18:01:55 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/23 18:18:06 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/23 22:06:01 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	hstack = (char *)haystack;
 	if (!*needle)
 		return (hstack);
-	while (*needle && len)
+	while (needle[reset_len] && len)
 	{
 		if (!*hstack)
 			break ;
-		else if (*hstack == *needle && reset_len++ && needle++)
-			;
+		else if (*hstack == needle[reset_len])
+			reset_len++;
 		else if (reset_len)
-		{
-			needle = needle - reset_len;
 			reset_len = 0;
-		}
 		hstack++;
 		len--;
 	}
-	if (reset_len && !*needle)
+	if (reset_len && !needle[reset_len])
 		return (hstack - reset_len);
 	return ((void *)0);
 }
--- srcs/ft_strstr.c
+++ srcs/ft_strstr.c
@@ -6,34 +6,30 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/15 16:14:39 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/23 17:57:40 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/23 22:08:48 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strstr(char *haystack, char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int reset_len;
+	int		reset_len;
+	char	*hstack;
 
 	reset_len = 0;
-	if (*needle == '\0')
-		return (haystack);
-	while (*needle)
+	hstack = (char *)haystack;
+	if (!*needle)
+		return (hstack);
+	while (needle[reset_len])
 	{
-		if (*haystack == '\0')
+		if (!*hstack)
 			break ;
-		else if (*haystack == *needle)
-		{
+		else if (*hstack == needle[reset_len])
 			reset_len++;
-			needle++;
-		}
 		else if (reset_len)
-		{
-			needle = needle - reset_len;
 			reset_len = 0;
-		}
-		haystack++;
+		hstack++;
 	}
-	if (reset_len && *needle == '\0')
-		return (haystack - reset_len);
+	if (reset_len && !needle[reset_len])
+		return (hstack - reset_len);
 	return ((void *)0);
 }

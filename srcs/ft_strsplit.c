59bc40ff7c062c3fbea77b2a3bd95bbeb32402b1
Jacob Polloreno <jpollore@e1z3r5p14.42.us.org>
2018-02-28 15:30:28 -0800

    reimplement strsplit

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -6,113 +6,59 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 12:56:34 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/28 12:56:38 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/28 15:28:32 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_list.h"
 
-static t_list	*create_elem_length(int len)
+static size_t	get_word_cnt(const char *str, char c)
 {
-	t_list *node;
+	size_t words;
+	size_t len;
 
-	if ((node = (t_list *)ft_memalloc(sizeof(*node))))
-	{
-		node->content = &len;
-		node->next = 0;
-	}
-	return (node);
-}
-
-static int		remove_init(const char *str, char c)
-{
-	int len;
-
+	if (!*str)
+		return (0);
 	len = 0;
-	while (str[len] &&
-			(str[len] == c) &&
-			(str[len + 1] == c))
-		len++;
-	return (len);
-}
-
-static t_list	*get_word_cnt(int *words, const char *str, char c)
-{
-	int		len;
-	int		sub_len;
-	t_list	*head;
-	t_list	*cursor;
-
-	len = remove_init(str, c);
-	while (str[len] || !(str[len + 1]))
-	{
-		if ((!(str[len + 1] == c) && (str[len] == c)) || (len == 0))
-		{
-			sub_len = 0;
-			while (str[++len] && !(str[len] == c))
-				sub_len++;
-			if ((*words)++ && (cursor->next = create_elem_length(sub_len)))
-				cursor = cursor->next;
-			else if ((head = create_elem_length(sub_len)))
-				cursor = head;
-			else
-				return (NULL);
-		}
-		else
-			len++;
-	}
-	return (head);
-}
-
-static void		put_words(char **res, const char *str, char c)
-{
-	int	len;
-	int	word;
-	int	sub_len;
-
-	len = remove_init(str, c);
-	word = 0;
+	words = 0;
 	while (str[len])
 	{
-		if (!str[len + 1])
-			return ;
-		else if ((!(str[len + 1] == c) && str[len] == c) || (len == 0))
+		if (str[len] != c)
 		{
-			sub_len = 0;
-			if (len != 0 || str[len] == c)
+			while (str[len] && (str[len] != c))
 				len++;
-			while (str[len] && !(str[len] == c))
-				*(*(res + word) + sub_len++) = *(str + len++);
-			*(*(res + (word++)) + sub_len) = '\0';
+			words++;
 		}
 		else
 			len++;
 	}
+	return (words);
 }
 
 char			**ft_strsplit(const char *str, char c)
 {
-	int		words;
-	int		i;
-	char	**res;
-	t_list	*arr;
-	t_list	*tmp;
+	unsigned int		len;
+	unsigned int		sub_len;
+	char				**res;
+	size_t				words;
+	size_t				current_word;
 
-	i = 0;
-	words = 0;
-	if (!(arr = get_word_cnt(&words, str, c)))
+	words = get_word_cnt(str, c);
+	if (!(res = (char **)ft_memalloc(sizeof(*res) * words + 1)))
 		return (NULL);
-	res = (char **)ft_memalloc(sizeof(char*) * (words + 1));
-	while (i < words)
+	current_word = 0;
+	len = 0;
+	while (current_word < words)
 	{
-		res[i] = (char *)ft_memalloc(sizeof(char) * *(int *)arr->content + 1);
-		i++;
-		tmp = arr;
-		arr = arr->next;
-		free(tmp);
+		while (str[len] == c)
+			len++;
+		sub_len = 0;
+		while (str[len + sub_len] && (str[len + sub_len] != c))
+			sub_len++;
+		res[current_word++] = ft_strsub(str, len, sub_len);
+		len += sub_len;
 	}
-	put_words(res, str, c);
-	res[i] = 0;
+	res[current_word] = ((void *)0);
 	return (res);
 }

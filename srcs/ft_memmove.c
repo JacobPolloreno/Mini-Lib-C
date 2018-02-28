93228c050841d8c4d9223d7a623905b8b91e1938
Jacob Polloreno <jpollore@e1z3r5p10.42.us.org>
2018-02-27 20:39:32 -0800

    bug fix for memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 16:08:16 by jpollore          #+#    #+#             */
-/*   Updated: 2018/02/21 16:51:34 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/02/27 20:38:26 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char *buffer;
+	char *d;
+	char *s;
 
-	buffer = (char *)malloc(sizeof(*buffer) * len);
-	ft_memcpy(buffer, src, len);
-	ft_memcpy(dst, buffer, len);
-	return (dst);
+	d = (char *)dst;
+	s = (char *)src;
+	if (s < d)
+	{
+		while (len--)
+			d[len] = s[len];
+	}
+	else
+		ft_memcpy(d, s, len);
+	return (d);
 }

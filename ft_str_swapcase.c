60dc41139067c5d42d979aa8d05c38f60b77b23a
Jacob Polloreno <jpollore@e1z3r5p14.42.us.org>
2018-03-01 08:59:36 -0800

    add extra functions

--- ft_str_swapcase.c
+++ ft_str_swapcase.c
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 08:49:26 by jpollore          #+#    #+#             */
-/*   Updated: 2018/03/01 08:53:47 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/03/01 08:59:30 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_swapcase(const char *s)
 	char	ch;
 
 	len = ft_strlen(s);
-	if ((swapped = (char *)ft_memalloc(sizeof(*swapped) * len)))
+	if ((swapped = (char *)ft_memalloc(sizeof(*swapped) * len + 1)))
 	{
 		len = 0;
 		while (s[len])
@@ -31,6 +31,7 @@ char	*ft_swapcase(const char *s)
 				ch = ch - 32;
 			swapped[len++] = ch;
 		}
+		swapped[len] = '\0';
 		return (swapped);
 	}
 	return (NULL);

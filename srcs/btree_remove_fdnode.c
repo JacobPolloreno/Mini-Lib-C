8c272056f470cb8a9dcd4765f43e40c354dc2943
Jacob Polloreno <makaveliix16@gmail.com>
2018-03-23 18:02:04 -0700

    improve binary tree functionality with files

--- srcs/btree_remove_fdnode.c
+++ srcs/btree_remove_fdnode.c
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 17:51:48 by jpollore          #+#    #+#             */
-/*   Updated: 2018/03/23 17:57:37 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/03/23 18:01:48 by jpollore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ t_btree	*btree_remove_fdnode(t_btree **root, void *data_ref)
 {
 	t_btree_fns	*fns;
 
-	fns->cmpf = &fdsearchcmp;
-	fns->delf = &btree_deletefd;
-	fns->updatef = &btree_updatefd;
-	return (btree_remove_node(root, data_ref, fns));
+	if ((fns = (t_btree_fns *)ft_memalloc(sizeof(*fns))))
+	{
+		fns->cmpf = &fdsearchcmp;
+		fns->delf = &btree_deletefd;
+		fns->updatef = &btree_updatefd;
+		return (btree_remove_node(root, data_ref, fns));
+	}
+	return (NULL);
 }

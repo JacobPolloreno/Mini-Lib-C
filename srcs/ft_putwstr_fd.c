09256df7a49106b07ee55b44cbd350da0a7c718c
Jacob Polloreno <makaveliix16@gmail.com>
2018-05-10 14:13:16 -0700

    norm files

--- srcs/ft_putwstr_fd.c
+++ srcs/ft_putwstr_fd.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putwstr_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/10 14:11:50 by jpollore          #+#    #+#             */
+/*   Updated: 2018/05/10 14:12:04 by jpollore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_putwstr_fd(const t_char32 *str, int fd)
 {
 	t_char32 *ptr;
 
-	ptr = (t_char32 *) str;
+	ptr = (t_char32 *)str;
 	while (*ptr)
 		ft_putwchar_fd((*ptr)++, fd);
 }

ca2009139a122dbf2ff01b866f2586511223de30
Jacob Polloreno <makaveliix16@gmail.com>
2018-05-21 14:59:20 -0700

    add ultoa_base and remove powll

---
+++ srcs/ft_ultoa_base.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uitoa_base.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/25 14:44:19 by jpollore          #+#    #+#             */
+/*   Updated: 2018/05/19 15:41:37 by jpollore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Convert unsigned integer ASCII decimal-digit string in base
+*/
+
+static char	digit_to_char(int nb)
+{
+	if (nb >= 10)
+		return (nb - 10 + 'a');
+	else
+		return (nb + '0');
+}
+
+char		*ft_ultoa_base(unsigned long long n, int base)
+{
+	char	*res;
+	int		len;
+
+	len = 1;
+	while (ft_powul(base, len) - 1 < n)
+		len++;
+	len = len > 20 ? 20 : len;
+	if (!(res = (char *)ft_memalloc(sizeof(*res) * ++len)))
+		return (NULL);
+	res[--len] = '\0';
+	while (len-- > 0)
+	{
+		res[len] = digit_to_char(n % base);
+		n /= base;
+	}
+	return (res);
+}
